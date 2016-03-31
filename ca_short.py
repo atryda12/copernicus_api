4513db1ffaac33d429239dc79e3f1780310703d9
Damian Sepielak <damian.sepielak@gmail.com>
2016-03-31 11:04:10 +0200

    short - requesting in helper

--- ca_short.py
+++ ca_short.py
@@ -9,27 +9,28 @@ MOTION = copernicus_api.QueryableComponent.MOTION
 
 
 def set_dash(angle):
-    copernicus_api.Request.to_set_dashboard_angle(angle)
+    req = copernicus_api.Request.to_set_dashboard_angle(angle)
+    copernicus_api.Copernicus().send_request(req)
 
 
 def set_led1(value):
-    copernicus_api.Request.to_set_led1(value)
+    req = copernicus_api.Request.to_set_led1(value)
+    copernicus_api.Copernicus().send_request(req)
 
 
 def set_led2(r, g, b):
-    copernicus_api.Request.to_set_rgb_of_led2(copernicus_api.Rgb3(r, g, b))
+    req = copernicus_api.Request.to_set_rgb_of_led2(copernicus_api.Rgb3(r, g, b))
+    copernicus_api.Copernicus().send_request(req)
 
 
 def subscribe(*args):
-    copernicus_api.Request.for_automatic_updates_of(args)
+    req = copernicus_api.Request.for_automatic_updates_of(args)
+    copernicus_api.Copernicus().send_request(req)
 
 
 def query(*args):
-    copernicus_api.Request.query_for_parameters_of(args)
-
-
-def req(request):
-    copernicus_api.Copernicus().send_request(request)
+    req = copernicus_api.Request.query_for_parameters_of(args)
+    copernicus_api.Copernicus().send_request(req)
 
 
 def get_res():
